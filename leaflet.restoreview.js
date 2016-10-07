b32db8e51891019d20600f54d3b7ba6be203532c
Enguerran Colson <engcolson@gmail.com>
2016-10-07 09:24:00 +0200

    refactor code

--- leaflet.restoreview.js
+++ leaflet.restoreview.js
@@ -1,32 +1,33 @@
 (function() {
     var RestoreViewMixin = {
         restoreView: function () {
-            if (storageAvailable('localStorage')) {
-                var storage = window.localStorage || {};
-                if (!this.__initRestore) {
-                    this.on('moveend', function (e) {
-                        if (!this._loaded)
-                            return;  // Never access map bounds if view is not set.
+            if (!storageAvailable('localStorage')) {
+                return false;
+            }
+            var storage = window.localStorage;
+            if (!this.__initRestore) {
+                this.on('moveend', function (e) {
+                    if (!this._loaded)
+                        return;  // Never access map bounds if view is not set.
 
-                        var view = {
-                            lat: this.getCenter().lat,
-                            lng: this.getCenter().lng,
-                            zoom: this.getZoom()
-                        };
-                        storage['mapView'] = JSON.stringify(view);
-                    }, this);
-                    this.__initRestore = true;
-                }
+                    var view = {
+                        lat: this.getCenter().lat,
+                        lng: this.getCenter().lng,
+                        zoom: this.getZoom()
+                    };
+                    storage['mapView'] = JSON.stringify(view);
+                }, this);
+                this.__initRestore = true;
+            }
 
-                var view = storage['mapView'];
-                try {
-                    view = JSON.parse(view || '');
-                    this.setView(L.latLng(view.lat, view.lng), view.zoom, true);
-                    return true;
-                }
-                catch (err) {
-                    return false;
-                }
+            var view = storage['mapView'];
+            try {
+                view = JSON.parse(view || '');
+                this.setView(L.latLng(view.lat, view.lng), view.zoom, true);
+                return true;
+            }
+            catch (err) {
+                return false;
             }
         }
     };
